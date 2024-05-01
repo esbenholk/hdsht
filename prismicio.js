575ba956b579304b61681a400806b11837fe3a2a
esben holk nielsen <esbenholknielsen@gmail.com>
2024-05-01 12:35:59 +0200

    fetch w produktion

--- prismicio.js
+++ prismicio.js
@@ -35,6 +35,11 @@ export const createClient = (config = {}) => {
     routes,
     ...config,
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    fetchOptions:
+      process.env.NODE_ENV === "production"
+        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
+        : { next: { revalidate: 5 } },
+    ...config,
 
   });
  
