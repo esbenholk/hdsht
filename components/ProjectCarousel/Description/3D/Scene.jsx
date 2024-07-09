0fb1d43c8d9f470f7be01efa30df0bb354825406
esbenholk <esbenholknielsen@gmail.com>
2024-07-09 14:40:33 +0200

    react three drei out

--- components/ProjectCarousel/Description/3D/Scene.jsx
+++ components/ProjectCarousel/Description/3D/Scene.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, Environment } from "@react-three/drei";
+// import { OrbitControls, Environment } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { MotionCanvas } from "framer-motion-3d";
 import Phone from "./Phone/Phone";
@@ -7,14 +7,14 @@ const Scene = ({ slice, modal }) => {
     <Canvas>
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.15} penumbra={1} />
-      <OrbitControls
+      {/* <OrbitControls
         enableZoom={false}
         enablePan={false}
         minAzimuthAngle={-Math.PI / 4}
         maxAzimuthAngle={Math.PI / 4}
         minPolarAngle={Math.PI / 2}
         maxPolarAngle={Math.PI / 2}
-      />
+      /> */}
       <Phone slice={slice} modal={modal} />
     </Canvas>
   );
